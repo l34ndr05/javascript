21afc17b91cce7c6c5e1acf139ece1564e25e72e
l34ndr05 <leandro.grasimo@gmail.com>
2023-11-10 09:25:33 -0300

    Updating files

--- javascript.js
+++ javascript.js
@@ -4,3 +4,47 @@ a = 11 % 4
 console.log(a)
 
 // if the remiander of division equals zero, it means it`a an even number
+
+//Comtound assingment with Augmented Addition
+
+var au = 3;
+var bu = 17;
+var cu = 12;
+
+// Only modifiy code belo this line
+
+au += 12; // a = a +12
+bu += 0;  // b = b + 8
+cu += 7; // c =c + 7 thats a shortcut
+
+  //minus equals do the same thing
+
+au -= 12; // a = a - 12
+bu -= 0;  // b = b - 8
+cu -= 7; // c =c - 7 thats a shortcut
+
+// Compound assingment with augmenete Multiplication
+
+au *= 12; // a = a * 12
+bu *= 0;  // b = b * 8
+cu *= 7; // c = c * 7 
+
+// Compound assingment with augmenete Division
+
+au /= 12; // a = a / 12
+bu /= 0;  // b = b / 8
+cu /= 7; // c = c / 7 
+
+// Declare String Variables
+
+//  Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+var myFirstName = 'Larry';
+var myLastName = 'Pagination';
+
+// Escape Literal quotes
+ var myStr = "I am a \"double quote" string inside a \"double quote""
+ 
+console.log(myStr);
